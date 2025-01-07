51fb56bf3a82f21cd1548ae8c92f5391db0a6f71
Viktor Arohlén <varohlen@gmail.com>
2025-01-07 10:23:31 +0100

    added server renderings on comparisonpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',  // This is the default, allows per-page server rendering
+  adapter: cloudflare({
+    mode: 'directory'
+  }),
   integrations: [tailwind()],
 });
